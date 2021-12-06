d32f35674063e868bd1fcc745d0847f1d0c15fe8
rogersaya26 <rogersaya@gmail.com>
2021-12-06 12:09:25 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Quote from "./components/Quote";
+import Spinner from "./components/Spinner";
+
+const initialQuote = {
+  text: "Quote",
+  author: "Author",
+};
 
 function App() {
+  const [quote, setQuote] = useState(initialQuote);
+  const [loading, setLoading] = useState(false);
+
+  //useefect para arrancar su primera renderizada automatica
+  //use efect no acepta async acincronismo en sus argumentos
+  // se declara en la funcion es asinc y antes del fetch se llama await
+
+  const updateQuote = async () => {
+    setLoading(true);
+    const url = "https://www.breakingbadapi.com/api/quote/random";
+    const res = await fetch(url);
+    const [newQuote] = await res.json();
+    const { quote: text, author } = newQuote;
+    setQuote({
+      text,
+      author,
+    });
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    updateQuote();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/7/77/Breaking_Bad_logo.svg"
+        alt="logo breaking bad"
+      />
+      <button onClick={() => updateQuote()}>Get Another</button>
+
+      {loading ? <Spinner /> : <Quote quote={quote} />}
     </div>
   );
 }
